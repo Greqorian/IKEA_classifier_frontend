d3a4e0e35676eed3388952baf4f56d9153567064
Greq <greqorian@gmail.com>
2022-02-25 23:48:06 +0100

    fix:

--- src/components/StartPage.jsx
+++ src/components/StartPage.jsx
@@ -10,7 +10,7 @@ const StartPage = ({ handleChange, fetchResult, img, currentStep, response }) =>
 
 
     currentStep === 3 ?
-        results = 
+        results =
         <div className="row spinner">
             <div className="col-12 center-div">
                 <button className="btn btn-primary" type="button" disabled>
@@ -19,35 +19,35 @@ const StartPage = ({ handleChange, fetchResult, img, currentStep, response }) =>
                 </button>
             </div>
         </div>
-        : currentStep === 1 ? results = <ResultPage result={response} /> 
-        : results = <div></div>
+        : currentStep === 1 ? results = <ResultPage result={response} />
+            : results = <div></div>
 
 
     return (
         <div className="container">
-            <div className="row">
-                <h1 className="col-12 header">
-                    IKEA Classifier
-                </h1>
-            </div>
-            <div className="row">
-                <div className="col-12 message">
-                    This simple flower classifier can identify the following flowers using their image:
-                    Daisy, Dandelion, Rose, Sunflower and Tulip.
-                    Upload your image below:
+            <div className="frame">
+                <div className="row">
+                    <h1 className="col-12 header">
+                        IKEA Classifier
+                    </h1>
                 </div>
-            </div>
-            <div className="row">
-                <div className="col-12 center-div choose-file">
-                    <input type="file" name="file" id="file" className="choose" onChange={(event) => { console.log(event.target.files[0]); handleChange(event.target.files[0]) }} />
-                    <label htmlFor="file">Choose a file</label>
+                <div className="row">
+                    <div className="col-12 message">
+                        IKEA 
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-12 center-div choose-file">
+                        <input type="file" name="file" id="file" className="choose" onChange={(event) => { console.log(event.target.files[0]); handleChange(event.target.files[0]) }} />
+                        <label htmlFor="file">UPLOAD IMAGE</label>
 
+
+                    </div>
 
                 </div>
-
+                {image}
+                {results}
             </div>
-            {image}
-            {results}
         </div>
     );
 }
