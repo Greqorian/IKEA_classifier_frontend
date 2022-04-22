46bdd5cd02e53fa1e97ca7ba78073b924169e7d0
Greq <greqorian@gmail.com>
2022-04-22 19:02:38 +0200

    feat: add global styles

---
+++ src/styles/globalStyles.js
@@ -0,0 +1,15 @@
+const globalStyles = {
+
+    shadow: {
+        boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 8px',
+    },
+    colors:{
+        header: '#7b9eae',
+        background: '#fff',
+        container: '#fff',
+        button:  '#fff',
+    }
+    
+
+
+}
